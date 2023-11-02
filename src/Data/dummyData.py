f38fd8de6b40fd25ea4e388acbb0f7dda63a4849
gtcha2 <aron7628@gmail.com>
2023-11-02 14:08:55 -0500

    file structure updateas

---
+++ src/Data/dummyData.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+# data diagram should be in the format of what?
+
+# this file should be building the following, 
+
+# data should consist of the following, imageID, 
+
+def main():
+    if __name__ == "__main__":
+        main()
+        
